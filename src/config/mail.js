df7d0ca654cc6b413bd29ebeedeb82544780e2fb
Cristiano Ribeiro <cristianoribeiro@Cristianos-MacBook-Pro.local>
2020-04-28 17:01:10 -0300

    all

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '640bcf5dd347ea',
+    pass: '6d4ee0d5ee4159',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com.br>',
+  },
+};
---
+++ src/routes.js
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controller/UserController';
+import SessionController from './app/controller/SessionController';
+import FileController from './app/controller/FileController';
+import ProviderController from './app/controller/ProviderController';
+import AppointmentController from './app/controller/AppointmentController';
+import ScheduleController from './app/controller/ScheduleController';
+import NotificationController from './app/controller/NotificationController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notification', NotificationController.index);
+routes.put('/notification/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
