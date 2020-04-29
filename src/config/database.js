c111cf84fea501c6feace273f340e94358fa3cbd
Cristiano Ribeiro <cristianoribeiro@Cristianos-MacBook-Pro.local>
2020-04-29 16:39:16 -0300

    update

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'gobarber',
+  database: 'papiro',
   define: {
     timestamps: true,
     underscored: true,
